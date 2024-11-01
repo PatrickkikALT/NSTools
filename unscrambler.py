5ab7795b4518a49e7c8f8c53b6ebc9f20ae7e4df
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2024-11-01 17:26:18 +0100

    changes

--- unscrambler.py
+++ unscrambler.py
@@ -1,4 +1,16 @@
-stations = open('stations.json', 'r')
+import os
+import requests
+if not os.path.exists("stations.json"):
+    url = "https://gist.githubusercontent.com/PatrickkikALT/2a597bebb4a3f16138fff82ae56a99ce/raw/e45bbb89a7b3852b1f1317edc3f6e12ab3c14023/stations.json"
+    response = requests.get(url)
+    json = response.text
+    with open("stations.json", "w", encoding="utf-8") as f:
+        for station in json:
+            f.write(station)
+stations = []
+for s in open("stations.json", "r", encoding="utf-8"):
+    stations.append(s.strip())
+print("Puzzel:")
 scrambledStation = input()
 
 filteredStations = []
